3530eb47acb18eac9718abcafc84a6153744e751
Juliano Cezar Teles Vaz <julianoctvaz@gmail.com>
2021-01-17 12:38:11 -0300

    feat(UI):Fisrt navigation page welcome to OnboardinGender

--- inDica/inDica/Screens/Welcome.swift
+++ inDica/inDica/Screens/Welcome.swift
@@ -15,6 +15,8 @@ struct WelcomeView: View {
     var description = "Queremos te apresentar alguém para te ajudar a arranjar um emprego. Alguém real como você, que passou pelas mesmas situações e agora está cheia de dicas para dar."
     var description2 = "Para isso, seria ótimo te conhecer..."
     
+    @State var showOnboardingGender_Toggle = false
+    
     @EnvironmentObject var user: UserSettings //deixar essa classe global com os dados do usario
     
     var body: some View {
@@ -62,9 +64,13 @@ struct WelcomeView: View {
             
             HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/){
                 Spacer()
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Image(systemName: "arrow.right")
-                        .padding(UIScreen.main.bounds.height*0.02)
+                Button(
+                    action: {
+                        self.showOnboardingGender_Toggle = true
+                },
+                    label: {
+                        Image(systemName: "arrow.right")
+                            .padding(UIScreen.main.bounds.height*0.02)
                 })
                 .foregroundColor(.white)
                 .background(Color.init(red: 0.3451, green: 0.337255, blue: 0.84)) // Roxo mais escuro: 5856D6 = (88, 86, 214)))
@@ -72,6 +78,11 @@ struct WelcomeView: View {
             }
             .frame(width: UIScreen.main.bounds.width*0.8, height: UIScreen.main.bounds.height*0.1, alignment: .center)
             .padding(.leading)
+            .fullScreenCover(isPresented: $showOnboardingGender_Toggle) {
+                if(user.username != ""){
+                    FamilyPerCapitaIncomeView()
+                }
+            }
             
             Spacer()
         }
